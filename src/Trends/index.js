94ee25c3b8bd31fefd9d417e0ffa9e050f802e3b
usmanmunara <usmanfarooqmunara@gmail.com>
2019-08-27 14:33:39 +0800

    trends bug fix

--- src/Trends/index.js
+++ src/Trends/index.js
@@ -6,30 +6,7 @@ const axios = require('axios');
 // const jwt = require('../Token');
 
 router.get('/', function getTrends(req, res) {
-  googleTrends.interestOverTime({keyword: req.body.keyword, startTime: new Date('January 01, 2019 00:00:00')})
-      .then(function(results) {
-        // console.log('These results are awesome', results);
-        const resultDATA = JSON.parse(results);
-        const resultsPromise = [];
-        resultsPromise.push(resultDATA.default.timelineData.map((trend) => {
-          // console.log(trend.formattedTime, trend.formattedValue[0]);
-          return {
-            time: trend.formattedTime,
-            value: trend.formattedValue[0],
-          };
-        }));
-        Promise.all(resultsPromise).then((finalResults) => {
-          res.send(finalResults[0]);
-        });
-        // res.send(resultDATA);
-      })
-      .catch(function(err) {
-        console.error('Oh no there was an error', err);
-      });
-});
-
-router.get('/', function getTrends(req, res) {
-  googleTrends.interestOverTime({keyword: req.params.keyword, startTime: new Date('January 01, 2019 00:00:00')})
+  googleTrends.interestOverTime({keyword: req.query.keyword, startTime: new Date('January 01, 2019 00:00:00')})
       .then(function(results) {
         // console.log('These results are awesome', results);
         const resultDATA = JSON.parse(results);
@@ -52,7 +29,7 @@ router.get('/', function getTrends(req, res) {
 });
 
 router.get('/related', function getTrends(req, res) {
-  googleTrends.relatedQueries({keyword: req.params.keyword})
+  googleTrends.relatedQueries({keyword: req.query.keyword})
       .then(function(results) {
         // console.log('These results are awesome', results);
         const resultDATA = JSON.parse(results);
@@ -76,11 +53,8 @@ router.get('/related', function getTrends(req, res) {
 });
 
 router.get('/relatedMuse', function getTrends(req, res) {
-  axios.get('https://api.datamuse.com/words', {
-    params: {
-      ml: req.params.keyword,
-    },
-  }).then((result) => {
+  console.log(req.query.keyword);
+  axios.get(`https://api.datamuse.com/words?ml=${req.query.keyword}`).then((result) => {
     res.send(result.data);
   })
       .catch(function(err) {
